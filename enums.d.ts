491926d3d05dfeecf1687f9a6096de00fe3ddd0e
Onion3 <Onion3@aliyun.com>
2020-10-22 01:44:01 +0800

    update

---
+++ enums.d.ts
@@ -0,0 +1,51 @@
+// Auto-generated. Do not edit.
+
+
+    declare const enum RemoteButton {
+      Power = 0x45,
+	  Up = 0x40,
+	  Left = 0x7,
+	  Right = 0x9,
+	  Down = 0x19,
+	  MENU = 0x47,
+	  A = 0x46,
+	  B = 0x44,
+	  C = 0x43,
+	  D = 0x0C,
+	  OK = 0x15,
+	  NUM0 = 0x19,
+	  NUM1 = 0x0C,
+	  NUM2 = 0x18,
+	  NUM3 = 0x5E,
+	  NUM4 = 0x8,
+	  NUM5 = 0x1C,
+	  NUM6 = 0x5A,
+	  NUM7 = 0x42,
+	  NUM8 = 0x52,
+	  NUM9 = 0x4A,	 
+    }
+
+
+    declare const enum Pins {
+    P0 = 3,
+    P1 = 2,
+    P2 = 1,
+    P3 = 4,
+    P4 = 5,
+    P5 = 17,
+    P6 = 12,
+    P7 = 11,
+    P8 = 18,
+    P9 = 10,
+    P10 = 6,
+    P11 = 26,
+    P12 = 20,
+    P13 = 23,
+    P14 = 22,
+    P15 = 21,
+    P16 = 16,
+    P19 = 0,
+    P20 = 30,
+    }
+
+// Auto-generated. Do not edit. Really.
